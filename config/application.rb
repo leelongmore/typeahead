57d2610e3acf543d92731ef4381e6fdcdc4b5bb8
Lee Longmore <lee.longmore@gmail.com>
2017-12-15 11:11:34 +0000

    Install and configure rspec

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,14 @@ module Typeahead
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        controller_specs: false,
+        helper_specs: false,
+        request_specs: true,
+        routing_specs: false,
+        view_specs: false
+    end
   end
 end
